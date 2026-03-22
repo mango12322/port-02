4d1b52ef81e6b91f6894de4b8c171fee728eab7d
BangSungHyen <xpfl7920@gmail.com>
2026-03-23 00:53:10 +0900

    mobile complete

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Swiper
 const visualSwiper = new Swiper(".visual-swiper", {
   loop: true,
   slidesPerView: 1,
@@ -52,13 +53,7 @@ const storeSwiper = new Swiper(".store-slider", {
   },
 });
 
-const dropdownToggle = document.querySelector(".dropdown-toggle");
-const dropdown = document.querySelector(".dropdown");
-
-dropdownToggle.addEventListener("click", () => {
-  dropdown.classList.toggle("on");
-});
-
+// banner remve
 const banner = document.querySelector(".banner");
 const closeBox = document.querySelector("#check");
 const closeBtn = document.querySelector(".banner-close span");
@@ -73,4 +68,103 @@ closeBtn.addEventListener("click", () => {
   if (closeBox.checked) {
     localStorage.setItem("hideBanner", "true");
   }
+});
+
+// dropdownToggle
+const dropdownToggle = document.querySelector(".dropdown-toggle");
+const dropdown = document.querySelector(".dropdown");
+
+dropdownToggle.addEventListener("click", () => {
+  dropdown.classList.toggle("on");
+});
+
+// scroll
+const navbox = document.querySelector(".navbox");
+const navTop = navbox.offsetTop;
+let lastScrollY = window.scrollY;
+const bottomNav = document.querySelector(".bottom-nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= navTop) {
+    navbox.classList.add("fixed");
+  } else {
+    navbox.classList.remove("fixed");
+  }
+
+  const currentScrollY = window.scrollY;
+  const windowHeight = window.innerHeight;
+  const documentHeight = document.documentElement.scrollHeight;
+
+  if (
+    currentScrollY <= 0 ||
+    currentScrollY + windowHeight >= documentHeight - 20
+  ) {
+    bottomNav.classList.remove("hide");
+  } else if (currentScrollY > lastScrollY) {
+    bottomNav.classList.add("hide");
+  } else {
+    bottomNav.classList.remove("hide");
+  }
+
+  lastScrollY = currentScrollY;
+});
+
+// side-menu
+const menuBtn = document.querySelector(".menu-btn");
+const sideMenu = document.querySelector(".side-menu");
+const overlay = document.querySelector(".side-overlay");
+const sideCloseBth = document.querySelector(".side-close");
+const bottomOnBtn = document.querySelector(".open-side");
+
+menuBtn.addEventListener("click", () => {
+  sideMenu.classList.add("active");
+  overlay.classList.add("active");
+  document.body.style.overflow = "hidden";
+});
+
+overlay.addEventListener("click", () => {
+  sideMenu.classList.remove("active");
+  overlay.classList.remove("active");
+  document.body.style.overflow = "unset";
+});
+
+sideCloseBth.addEventListener("click", () => {
+  sideMenu.classList.remove("active");
+  overlay.classList.remove("active");
+});
+bottomOnBtn.addEventListener("click", () => {
+  sideMenu.classList.add("active");
+  overlay.classList.add("active");
+  document.body.style.overflow = "hidden";
+});
+
+const sideBtns = document.querySelectorAll(".side-btn");
+
+sideBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.classList.toggle("active");
+
+    const dropmenu = btn.parentElement.querySelector(".side-dropmenu");
+    if (dropmenu) {
+      dropmenu.classList.toggle("open");
+    }
+  });
+});
+
+// go-top
+const topBtn = document.getElementById("back-to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    topBtn.classList.add("show");
+  } else {
+    topBtn.classList.remove("show");
+  }
+});
+
+topBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 });
